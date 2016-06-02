7d37739a73363d091aa627b57b41208a6f20b47b
Faby <efaby10@gmail.com>
2016-06-02 16:46:51 -0500

    nosotrso servicios

--- templates/at_medical/html/mod_login/default_logout.php
+++ templates/at_medical/html/mod_login/default_logout.php
@@ -22,7 +22,7 @@ JHtml::_('behavior.keepalive');
 		echo JText::sprintf('MOD_LOGIN_HINAME', htmlspecialchars($user->get('username')));
 	} endif; ?>
 		<div class="logout-button">
-			<input type="submit" name="Submit" class="btn" value="<?php echo JText::_('JLOGOUT'); ?>" />
+			<input type="submit" name="Submit" class="btn" value="Salir" />
 			<input type="hidden" name="option" value="com_users" />
 			<input type="hidden" name="task" value="user.logout" />
 			<input type="hidden" name="return" value="<?php echo $return; ?>" />
